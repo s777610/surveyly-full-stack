4c94dc858b6417c934c6569bdda075c9cee09898
s777610 <s777610@gmail.com>
2018-12-23 10:49:19 -0800

    improve landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -3,8 +3,16 @@ import React from "react";
 const Landing = () => {
   return (
     <div style={{ textAlign: "center" }}>
-      <h1>Emaily!</h1>
+      <h1>Surveyly!</h1>
       Collect feedback form your users
+      <p>This is a demo project</p>
+      <p>
+        GitHub:{" "}
+        <a href="https://github.com/s777610/surveyly-full-stack">Click here!</a>
+      </p>
+      <p>
+        My personal site: <a href="https://weichenghung.com/">Click here!</a>
+      </p>
     </div>
   );
 };
